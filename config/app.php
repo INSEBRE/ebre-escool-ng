31c2d4002e8456fa78abeac9e284fc1139da5910
Roger Melich Curto <rogermelich@gmail.com>
2016-04-15 16:17:06 +0200

    Added laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //laravel-ide-helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
